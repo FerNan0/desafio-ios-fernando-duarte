2babdfed4381efd5249939752a01de9af206ed60
Fernando F Duarte <feernando.duarte@gmail.com>
2020-04-15 15:39:40 -0300

    ïnitial commit

---
+++ desafio-ios-fernando-duarte/Listagem/ListagemPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  ListagemPresenter.swift
+//  desafio-ios-fernando-duarte
+//
+//  Created by Fernando F Duarte on 15/04/20.
+//  Copyright © 2020 Fernando F Duarte. All rights reserved.
+//
+
+import Foundation
+
+class ListagemPresenter {
+    
+}
